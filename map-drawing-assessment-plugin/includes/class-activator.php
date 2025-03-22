8a73e1b35fdcdc81e324d7b66965ad81370515a7
Raihanrasul <rraihanraasull@gmail.com>
2025-03-22 04:41:07 +0000

    Add map-drawing-assessment-plugin/includes/class-activator.php 1742618466219

--- map-drawing-assessment-plugin/includes/class-activator.php
+++ map-drawing-assessment-plugin/includes/class-activator.php
@@ -8,6 +8,8 @@ class Map_Drawing_Assessment_Activator {
      * Activate the plugin
      */
     public static function activate() {
+        global $wpdb;
+
         // Create required database tables
         require_once plugin_dir_path(dirname(__FILE__)) . 'includes/class-questions.php';
         require_once plugin_dir_path(dirname(__FILE__)) . 'includes/class-submissions.php';
@@ -23,11 +25,18 @@ class Map_Drawing_Assessment_Activator {
             wp_mkdir_p($plugin_upload_dir);
         }
 
+        // Add capabilities
+        $admin_role = get_role('administrator');
+        if ($admin_role) {
+            $admin_role->add_cap('manage_map_assessment');
+            $admin_role->add_cap('view_map_assessment');
+        }
+
+        // Set default options
+        self::set_default_options();
+
         // Add plugin version to options
         add_option('map_drawing_assessment_version', MAP_DRAWING_ASSESSMENT_VERSION);
-
-        // Set default plugin options
-        self::set_default_options();
 
         // Clear permalinks
         flush_rewrite_rules();
@@ -46,9 +55,44 @@ class Map_Drawing_Assessment_Activator {
             'map_zoom_level' => 13,
             'map_center_lat' => 51.5074,
             'map_center_lng' => -0.1278,
+            'map_min_zoom' => 5,
+            'map_max_zoom' => 19,
+            'email_from_name' => get_bloginfo('name'),
+            'email_from_address' => get_bloginfo('admin_email'),
+            'access_code_length' => 8,
+            'access_token_expiry_days' => 2,
+            'enable_debug_logging' => false
         );
 
         foreach ($default_options as $key => $value) {
+            add_option('map_drawing_assessment_' . $key, $value);
+        }
+
+        // Email templates
+        $email_templates = array(
+            'access_code_subject' => __('Your Map Assessment Access Code', 'map-drawing-assessment'),
+            'access_code_body' => __(
+                "Hello,\n\n" .
+                "Your access code for the map assessment is: {access_code}\n\n" .
+                "This code will expire in {expiry_days} days.\n\n" .
+                "Please visit {assessment_url} to start your assessment.\n\n" .
+                "Best regards,\n{site_name}",
+                'map-drawing-assessment'
+            ),
+            'results_subject' => __('Your Map Assessment Results', 'map-drawing-assessment'),
+            'results_body' => __(
+                "Hello,\n\n" .
+                "Your map assessment has been reviewed.\n\n" .
+                "Total Score: {total_score}\n" .
+                "Time Taken: {time_taken}\n\n" .
+                "You can view your detailed results here: {results_url}\n" .
+                "This link will expire in {expiry_days} days.\n\n" .
+                "Best regards,\n{site_name}",
+                'map-drawing-assessment'
+            )
+        );
+
+        foreach ($email_templates as $key => $value) {
             add_option('map_drawing_assessment_' . $key, $value);
         }
     }
